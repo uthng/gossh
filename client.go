9f45d9ecfa063cabeabd212d068aca149315c85c
uthng <u.nguyenthanh@gmail.com>
2019-01-05 12:36:32 +0100

    Add session close and client test file

--- client.go
+++ client.go
@@ -1,45 +1,48 @@
 package ssh
 
 import (
-    //"fmt"
-    "bytes"
-    "strconv"
+	"bytes"
+	//"fmt"
+	"strconv"
 
-    "golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh"
 )
 
+// Client encapsulates ssh client
 type Client struct {
-    client      *ssh.Client
+	client *ssh.Client
 }
 
-// NewDockerClient initializes a docker client to remote cluster
-// following authentication configuration
+// NewClient initializes a ssh client following
+// authentication configuration
 func NewClient(config *Config) (*Client, error) {
-    c := &Client{}
+	c := &Client{}
 
-    client, err := ssh.Dial("tcp", config.Host + ":" + strconv.Itoa(config.Port), config.ClientConfig)
-    if err != nil {
-        return nil, err
-    }
+	client, err := ssh.Dial("tcp", config.Host+":"+strconv.Itoa(config.Port), config.ClientConfig)
+	if err != nil {
+		return nil, err
+	}
 
-    c.client = client
-    return c, nil
+	c.client = client
+	return c, nil
 }
 
+// ExecCommand executes a shell command on remote machine
 func (c *Client) ExecCommand(cmd string) ([]byte, error) {
-    var buf bytes.Buffer
+	var buf bytes.Buffer
 
-    session, err := c.client.NewSession()
-    if err != nil {
-        return nil, err
-    }
+	session, err := c.client.NewSession()
+	if err != nil {
+		return nil, err
+	}
+	defer session.Close()
 
-    session.Stdout = &buf
+	session.Stdout = &buf
 
-    err = session.Run(cmd)
-    if err != nil {
-        return nil, err
-    }
+	err = session.Run(cmd)
+	if err != nil {
+		return nil, err
+	}
 
-    return buf.Bytes(), nil
+	return buf.Bytes(), nil
 }
---
+++ client_test.go
@@ -0,0 +1,32 @@
+package ssh
+
+import (
+	//"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExecCommand(t *testing.T) {
+	sshUser := os.Getenv("SSH_USER")
+	sshPrivKey := os.Getenv("SSH_PRIVKEY")
+	sshHost := os.Getenv("SSH_HOST")
+
+	config, err := NewClientConfigWithKeyFile(sshUser, sshPrivKey, sshHost, 22, false)
+	if !assert.Nil(t, err) {
+		return
+	}
+
+	client, err := NewClient(config)
+	if !assert.Nil(t, err) {
+		return
+	}
+
+	assert.NotNil(t, client)
+
+	_, err = client.ExecCommand("ls")
+	assert.Nil(t, err)
+
+	//fmt.Println(string(res))
+}
