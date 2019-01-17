bc86708fe18c8db4391eba9617caf1cb64b315a3
uthng <u.nguyenthanh@gmail.com>
2019-01-17 01:29:56 +0100

    test(ssh/scp): Use gliderlabs ssh server in test units
    Closes #4

--- client_test.go
+++ client_test.go
@@ -1,19 +1,32 @@
 package gossh
 
 import (
-	//"fmt"
-	"os"
+	"fmt"
+	"io/ioutil"
+	//"os"
+	"strings"
 	"testing"
+
+	"github.com/gliderlabs/ssh"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestExecCommand(t *testing.T) {
-	sshUser := os.Getenv("SSH_USER")
-	sshPrivKey := os.Getenv("SSH_PRIVKEY")
-	sshHost := os.Getenv("SSH_HOST")
+func TestClientUserPass(t *testing.T) {
+	s := &ssh.Server{
+		Addr: ":2222",
+		Handler: func(s ssh.Session) {
+			fmt.Fprintf(s, "%s", strings.Join(s.Command(), " "))
+		},
+		PasswordHandler: func(ctx ssh.Context, password string) bool {
+			return ctx.User() == "user" && password == "pass"
+		},
+	}
 
-	config, err := NewClientConfigWithKeyFile(sshUser, sshPrivKey, sshHost, 22, false)
+	defer s.Close()
+	go s.ListenAndServe()
+
+	config, err := NewClientConfigWithUserPass("user", "pass", "localhost", 2222, false)
 	if !assert.Nil(t, err) {
 		return
 	}
@@ -25,20 +38,29 @@ func TestExecCommand(t *testing.T) {
 
 	assert.NotNil(t, client)
 
-	_, err = client.ExecCommand("ls")
+	cmd := "echo HelloWorld"
+	res, err := client.ExecCommand(cmd)
 	assert.Nil(t, err)
-
-	//fmt.Println(string(res))
+	assert.Equal(t, string(res), cmd)
 }
 
-func TestSCPBytes(t *testing.T) {
-	var content = []byte(`123455ototo totititititi`)
+func TestExecCommand(t *testing.T) {
+	s := &ssh.Server{
+		Addr: ":2222",
+		Handler: func(s ssh.Session) {
+			fmt.Fprintf(s, "%s", strings.Join(s.Command(), " "))
+		},
+		PublicKeyHandler: func(ctx ssh.Context, key ssh.PublicKey) bool {
+			data, _ := ioutil.ReadFile("./data/id_rsa.pub")
+			allowed, _, _, _, _ := ssh.ParseAuthorizedKey(data)
+			return ctx.User() == "user" && ssh.KeysEqual(key, allowed)
+		},
+	}
 
-	sshUser := os.Getenv("SSH_USER")
-	sshPrivKey := os.Getenv("SSH_PRIVKEY")
-	sshHost := os.Getenv("SSH_HOST")
+	defer s.Close()
+	go s.ListenAndServe()
 
-	config, err := NewClientConfigWithKeyFile(sshUser, sshPrivKey, sshHost, 22, false)
+	config, err := NewClientConfigWithKeyFile("user", "./data/id_rsa", "localhost", 2222, false)
 	if !assert.Nil(t, err) {
 		return
 	}
@@ -50,8 +72,8 @@ func TestSCPBytes(t *testing.T) {
 
 	assert.NotNil(t, client)
 
-	err = client.SCPBytes(content, "/tmp/titi/toto")
+	cmd := "echo HelloWorld"
+	res, err := client.ExecCommand(cmd)
 	assert.Nil(t, err)
-
-	//fmt.Println(string(res))
+	assert.Equal(t, string(res), cmd)
 }
